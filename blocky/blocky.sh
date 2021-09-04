b1a155585a1ffe9605cfb977e9c842808a3fe4b1
Evgeny Cheremnykh <rtzra@mail.ru>
2021-09-04 18:19:45 +0300

    Initial commit

---
+++ blocky/blocky.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# see https://github.com/0xERR0R/blocky
+# https://0xerr0r.github.io/blocky/
+
+listen_ip="10.20.30.1" # Interface IP for Blocky
+
+docker ps -a --no-trunc  | grep "blocky" | awk '{print $1}' | xargs docker container stop
+
+docker pull spx01/blocky
+
+echo y | docker system prune --volumes
+
+sudo cp blocky.yml /opt/blocky.yml
+
+sudo docker volume create blocky_logs
+sudo docker volume create block_blacklist
+
+docker run -d --restart unless-stopped \
+    -v blocky_blacklist:/app/blacklists/ \
+    -v blocky_logs:/logs \
+    --name blocky -v /opt/blocky.yml:/app/config.yml \
+    -p $listen_ip:4000:4000 \
+    -p $listen_ip:53:53/udp \
+    spx01/blocky
